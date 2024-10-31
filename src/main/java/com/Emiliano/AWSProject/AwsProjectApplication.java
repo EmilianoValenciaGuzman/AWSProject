abf613b8b7c11b2c9e5aa336f3a2eb657b54b8f4
Emiliano <evalencia142000@gmail.com>
2024-10-31 16:03:43 -0600

    Initial commit

---
+++ src/main/java/com/Emiliano/AWSProject/AwsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.Emiliano.AWSProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectApplication.class, args);
+		System.out.println("HOLA");
+	}
+
+}

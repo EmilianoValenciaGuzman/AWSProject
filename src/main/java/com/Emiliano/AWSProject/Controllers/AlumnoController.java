548a1e646f39a3c947886ecfc2237c0d85d3b98e
Emiliano <evalencia142000@gmail.com>
2024-11-10 14:11:13 -0600

    controllers y services de Alumno

--- src/main/java/com/Emiliano/AWSProject/Controllers/AlumnoController.java
+++ src/main/java/com/Emiliano/AWSProject/Controllers/AlumnoController.java
@@ -5,19 +5,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Emiliano.AWSProject.Entities.Alumno;
 import com.Emiliano.AWSProject.Services.AlumnoService;
 
-import javax.validation.Valid;
+import java.util.List;
+
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-@RestController // This annotation tells Spring that this class will be a controller
+
+@RestController 
 public class AlumnoController {
 
     @Autowired
@@ -35,20 +40,38 @@ public class AlumnoController {
     }
 
     @GetMapping("/alumnos")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public ResponseEntity<List<Alumno>> getAlumnos() {
+        return ResponseEntity.ok(alumnoService.getAlumnos());
     }
     
     @GetMapping("/alumnos/{id}")
-    public String getMethodName(@RequestParam String param, @RequestParam String param2) {
-        return new String();
+    public ResponseEntity<Alumno> getAlumnoById(@PathVariable String id) {
+        try {
+            return ResponseEntity.ok(alumnoService.getAlumnoById(Integer.parseInt(id)));
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
-    //     @GetMapping
-    // public ResponseEntity<List<Alumno>> obtenerAlumnos() {
-    //     return ResponseEntity.ok(alumnoService.getAlumnos());
-    // }
+    @PutMapping("alumnos/{id}")
+    public ResponseEntity<Alumno> updateAlumno(@Valid @RequestBody Alumno alumno, @PathVariable String id){
+        try {
 
+            return ResponseEntity.ok(alumnoService.actualizarAlumno(Integer.parseInt(id), alumno));
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 
-
+    @DeleteMapping("alumnos/{id}")
+    public ResponseEntity<String> deleteAlumno(@PathVariable String id){
+        try {
+            alumnoService.eliminarAlumno(Integer.parseInt(id));
+            return ResponseEntity.ok("Alumno con " +  id + " eliminado");
+        } catch (NumberFormatException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID inválido");
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Alumno no encontrado");
+        }
+    }
 }
--- src/main/java/com/Emiliano/AWSProject/Entities/Alumno.java
+++ src/main/java/com/Emiliano/AWSProject/Entities/Alumno.java
@@ -1,27 +1,26 @@
 package com.Emiliano.AWSProject.Entities;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
+
 
 public class Alumno {
     @NotNull(message = "El ID no puede ser nulo")
     private Integer id;
 
     @NotBlank(message = "El nombre no puede estar en blanco")
+    @NotNull(message = "El nombre no puede ser nulo")
     private String nombres;
 
-    @NotBlank(message = "El apellido no puede estar en blanco")
+    @NotNull(message = "El apellido no puede estar en blanco")
     private String apellidos;
 
     @NotNull(message = "La matrícula no puede ser nula")
-    @Pattern(regexp = "^[A-Za-z].*", message = "La matrícula debe comenzar con una letra")
+    @Pattern(regexp = "^[A-Za-z][0-9]+$", message = "La matrícula debe comenzar con una letra")
     private String matricula;
 
     @NotNull(message = "El promedio no puede ser nulo")
     @Positive(message = "El promedio debe ser un número positivo")
-    private Integer promedio;
+    private Double promedio;
 
 
     public Integer getId() { return id; }
@@ -36,6 +35,6 @@ public class Alumno {
     public String getMatricula() { return matricula; }
     public void setMatricula(String matricula) { this.matricula = matricula; }
 
-    public Integer getPromedio() { return promedio; }
-    public void setPromedio(Integer promedio) { this.promedio = promedio; }
+    public Double getPromedio() { return promedio; }
+    public void setPromedio(Double promedio) { this.promedio = promedio; }
 }
